91a578afb6ae12dee92bf90bae3f0c65085736eb
Lars Janssen <larsjanssen64@gmail.com>
2017-09-27 23:14:13 +0200

    Add configs

--- config/under-construction.php
+++ config/under-construction.php
@@ -2,7 +2,22 @@
 
 return [
     /**
-     * Activate under construction mode here.
+     * Activate under construction mode.
      */
     'enabled' => env('UNDER_CONSTRUCTION_ENABLED', true),
+
+    /**
+     * Enable throttle (max login attempts).
+     */
+    'throttle' => true,
+
+    /**
+     * If throttle is enabled (true) set the maximum number of attempts to allow.
+     */
+    'throttle_attempts' => 4,
+
+    /**
+     * If throttle is enabled (true) set the number of minutes to throttle for.
+     */
+    'throttle_minutes' => 5
 ];
