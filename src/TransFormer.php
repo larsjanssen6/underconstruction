9988fc5dab62ad0fb416b97615c3061ee9cac8dc
Lars Janssen <larsjanssen64@gmail.com>
2017-10-03 17:30:45 +0200

    validate for numbers

--- src/TransFormer.php
+++ src/TransFormer.php
@@ -6,10 +6,14 @@ use Exception;
 
 class TransFormer
 {
-    public function start($replace, $message)
+    public function transform($replace, $message)
     {
         if (strpos($message, '%i') === false) {
             throw new Exception('Make sure %i is in the sense');
+        }
+
+        if(1 === preg_match('~[0-9]~', $message)) {
+            throw new Exception('Make sure the sense does not contain numbers');
         }
 
         return str_replace('%i', $replace, $message);
