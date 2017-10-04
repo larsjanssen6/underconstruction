30bd672b3f3744f3c96c0c3a1eb6150be6560099
Lars Janssen <larsjanssen64@gmail.com>
2017-10-04 21:05:02 +0200

    Content change

--- src/TransFormer.php
+++ src/TransFormer.php
@@ -9,11 +9,11 @@ class TransFormer
     public function transform($replace, $message)
     {
         if (strpos($message, '%i') === false) {
-            throw new Exception('Make sure %i is in the sense');
+            throw new Exception('Make sure %i in your config file is in the sense');
         }
 
         if(1 === preg_match('~[0-9]~', $message)) {
-            throw new Exception('Make sure the sense does not contain numbers');
+            throw new Exception('Make sure the sense in your config file does not contain numbers');
         }
 
         return str_replace('%i', $replace, $message);
