84b84f5f895e2a41ae3fe149d0855f7e38cb88c8
Sebastien Pelletier <vesperknight@gmail.com>
2018-07-03 03:19:31 +0300

    Redirect to the intended url following a correct code entry

--- src/Controllers/CodeController.php
+++ src/Controllers/CodeController.php
@@ -57,7 +57,7 @@ class CodeController extends Controller
             'backButton'  => $this->config['back-button'],
             'showButton'  => $this->config['show-button'],
             'hideButton'  => $this->config['hide-button'],
-            'redirectUrl' => $this->config['redirect-url'],
+            'redirectUrl' => session()->get('intended.url', $this->config['redirect-url']),
         ]);
     }
 
