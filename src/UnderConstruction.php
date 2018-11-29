97428165d300c76d86c8ec4688328f734acac519
Lars Janssen <larsjanssen64@gmail.com>
2018-11-29 14:38:13 +0100

    Fix redirect bug

--- src/UnderConstruction.php
+++ src/UnderConstruction.php
@@ -30,6 +30,10 @@ class UnderConstruction
      */
     public function handle($request, Closure $next)
     {
+        if ($request->is('under/*')) {
+            return $next($request);
+        }
+
         if (! $this->config['enabled']) {
             return $next($request);
         }
